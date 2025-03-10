36a77642a33463e9b82a58ef0c4ce23edaeb77fd
fevse <fedulov.v.s@gmail.com>
2025-03-10 15:45:09 +0400

    add MongoDB

--- service/main.go
+++ service/main.go
@@ -5,50 +5,32 @@ import (
 	"context"
 	"log"
 	"net"
+	"time"
 
 	pb "service/sappgrpc"
+	"service/storage"
 
-	"github.com/gofrs/uuid"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 const port = ":50051"
 
-type server struct {
-	pb.UnimplementedProductInfoServer
-	productMap map[string]*pb.Product
-}
-
-func (s *server) AddProduct(ctx context.Context, in *pb.Product) (*pb.ProductID, error) {
-	out, err := uuid.NewV4()
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "Error while generating Product ID: %v", err)
-	}
-	in.Id = out.String()
-	if s.productMap == nil {
-		s.productMap = make(map[string]*pb.Product)
-	}
-	s.productMap[in.Id] = in
-	return &pb.ProductID{Value: in.Id}, status.New(codes.OK, "").Err()
-}
-
-func (s *server) GetProduct(ctx context.Context, in *pb.ProductID) (*pb.Product, error) {
-	value, exist := s.productMap[in.Value]
-	if exist {
-		return value, status.New(codes.OK, "").Err()
-	}
-	return nil, status.Errorf(codes.NotFound, "Product does not exist: %v", in.Value)
-}
-
 func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	stor := storage.NewProductService()
+	defer func() {
+		if err := stor.Disconnect(ctx); err != nil {
+			log.Fatalf("Failded to disconnect form MongoDB: %v", err)
+		}
+	}()
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatal("failed to listen: %w", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterProductInfoServer(s, &server{})
+	server := storage.NewProductService()
+	pb.RegisterProductInfoServer(s, server)
 	if err := s.Serve(lis); err != nil {
 		log.Fatal("failed to serve: %w", err)
 	}

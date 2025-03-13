39c8b1da7408142b2acac97cd835cd8fac77a54a
fevse <fedulov.v.s@gmail.com>
2025-03-13 16:27:44 +0400

    update orderService

--- orderService/client/main.go
+++ orderService/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
 	pb "orderService/client/orderService"
 	"time"
@@ -23,13 +24,98 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 
-	order1 := pb.Order{Id: "11", Items: []string{"Mooer Micro Looper"}, Destination: "Huul", Price: 45.0}
-	res, _ := client.AddOrder(ctx, &order1)
+	order1 := pb.Order{Id: "11", Items: []string{"Mooer Micro Looper"}, Destination: "Balmora", Price: 45.0}
+	res, err := client.AddOrder(ctx, &order1)
+	if err != nil {
+		log.Fatalf("cannot add order: %v", err)
+	}
 	if res != nil {
 		log.Print("AddOrder Response -> : ", res.Value)
 	}
 
 	retrievedOrder, err := client.GetOrder(ctx, &wrapperspb.StringValue{Value: "15"})
+	if err != nil {
+		log.Fatalf("cannot get order: %v", err)
+	}
 	log.Print("GetOrder Response -> : ", retrievedOrder)
 
+	searchStream, err := client.SearchOrders(ctx, &wrapperspb.StringValue{Value: "Boss"})
+	if err != nil {
+		log.Fatalf("cannot search orders: %v", err)
+	}
+
+	for {
+		searchOrder, err := searchStream.Recv()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatalf("cannot search order: %v", err)
+		}
+		log.Printf("Search Result : %v", searchOrder)
+	}
+
+	updOrder1 := pb.Order{Id: "12", Items: []string{"Coca-Cola Zero", "Big Mac"}, Destination: "Batumi"}
+	updOrder2 := pb.Order{Id: "14", Items: []string{"Sofa", "Table", "Chair"}, Destination: "Balmora"}
+	updOrder3 := pb.Order{Id: "16", Items: []string{"Holy Grail"}, Destination: "Erathia"}
+
+	updateStream, err := client.UpdateOrders(ctx)
+	if err != nil {
+		log.Fatalf("cannot update orders: %v", err)
+	}
+
+	if err := updateStream.Send(&updOrder1); err != nil {
+		log.Fatalf("cannot send order: %v", err)
+	}
+	if err := updateStream.Send(&updOrder2); err != nil {
+		log.Fatalf("cannot send order: %v", err)
+	}
+	if err := updateStream.Send(&updOrder3); err != nil {
+		log.Fatalf("cannot send order: %v", err)
+	}
+
+	updateRes, err := updateStream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("cannot close and recive: %v", err)
+	}
+	log.Printf("Update Orders Res : %s", updateRes)
+
+	streamProcOrder, err := client.ProcessOrders(ctx)
+	if err != nil {
+		log.Fatalf("cannot process orders: %v", err)
+	}
+	if err := streamProcOrder.Send(&wrapperspb.StringValue{Value: "12"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "12", err)
+	}
+	if err := streamProcOrder.Send(&wrapperspb.StringValue{Value: "13"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "13", err)
+	}
+	if err := streamProcOrder.Send(&wrapperspb.StringValue{Value: "14"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "14", err)
+	}
+
+	ch := make(chan struct{})
+
+	go asncClientBidirectionalRPC(streamProcOrder, ch)
+	time.Sleep(1 * time.Second)
+
+	if err := streamProcOrder.Send(&wrapperspb.StringValue{Value: "11"}); err != nil {
+		log.Fatalf("%v.Send(%v) = %v", client, "14", err)
+	}
+
+	if err := streamProcOrder.CloseSend(); err != nil {
+		log.Fatal(err)
+	}
+
+	ch <- struct{}{}
+}
+
+func asncClientBidirectionalRPC(streamProcOrder pb.OrderManagement_ProcessOrdersClient, c chan struct{}) {
+	for {
+		combinedShipment, errProcOrder := streamProcOrder.Recv()
+		if errProcOrder == io.EOF {
+			break
+		}
+		log.Printf("Combined shipment : %v", combinedShipment.OrderList)
+	}
+	<-c
 }
--- orderService/service/main.go
+++ orderService/service/main.go
@@ -3,15 +3,19 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"log"
 	"net"
 	pb "orderService/service/orderService"
+	"strings"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 )
 
 const port = ":50051"
+const orderBatchSize = 3
 
 var orderMap = make(map[string]*pb.Order, 0)
 
@@ -31,6 +35,85 @@ func (s *server) AddOrder(ctx context.Context, order *pb.Order) (*wrapperspb.Str
 	return &wrapperspb.StringValue{Value: "Order " + order.Id + " added"}, nil
 }
 
+func (s *server) SearchOrders(searchQuery *wrapperspb.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
+	for key, order := range orderMap {
+		// log.Printf("#%v : %v", key, order)
+		for _, itemStr := range order.Items {
+			if strings.Contains(itemStr, searchQuery.Value) {
+				err := stream.Send(order)
+				if err != nil {
+					return fmt.Errorf("error sending message to stream: %v", err)
+				}
+				log.Printf("Matching Order Found : %v", key)
+				break
+			}
+		}
+	}
+	return nil
+}
+
+func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) error {
+	oStr := strings.Builder{}
+	oStr.WriteString("Update Order IDs : ")
+	for {
+		order, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&wrapperspb.StringValue{Value: "Orders processed " + oStr.String()})
+		}
+		orderMap[order.Id] = order
+
+		log.Printf("Order #%v : Updated", order.Id)
+		oStr.WriteString(order.Id + ", ")
+	}
+}
+
+func (s *server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
+	batchMarker := 1
+	var combinedShipmentMap = make(map[string]*pb.CombinedShipment)
+	for {
+		orderId, err := stream.Recv()
+		if err == io.EOF {
+			for _, ship := range combinedShipmentMap {
+				if err := stream.Send(ship); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		destination := orderMap[orderId.GetValue()].Destination
+		shipment, ok := combinedShipmentMap[destination]
+
+		if ok {
+			ord := orderMap[orderId.GetValue()]
+			shipment.OrderList = append(shipment.OrderList, ord)
+			combinedShipmentMap[destination] = shipment
+		} else {
+			comShip := &pb.CombinedShipment{Id: orderMap[orderId.GetValue()].Destination, Status: "In progress"}
+			ord := orderMap[orderId.GetValue()]
+			comShip.OrderList = append(comShip.OrderList, ord)
+			combinedShipmentMap[destination] = comShip
+		}
+
+		if batchMarker == orderBatchSize {
+			for _, comb := range combinedShipmentMap {
+				log.Printf("Shipping : %v -> %v", comb.Id, len(comb.OrderList))
+				if err := stream.Send(comb); err != nil {
+					return err
+				}
+			}
+			batchMarker = 0
+			combinedShipmentMap = make(map[string]*pb.CombinedShipment)
+		} else {
+			batchMarker++
+		}
+	}
+}
+
 func main() {
 	initSampleData()
 	lis, err := net.Listen("tcp", port)
@@ -46,8 +129,8 @@ func main() {
 
 func initSampleData() {
 	orderMap["12"] = &pb.Order{Id: "12", Items: []string{"Fender Telecaster", "Fender Blues Junior"}, Destination: "Balmora", Price: 2500.00}
-	orderMap["13"] = &pb.Order{Id: "13", Items: []string{"Boss BD-2"}, Destination: "Sadrit Mora", Price: 140.00}
-	orderMap["14"] = &pb.Order{Id: "14", Items: []string{"Gibson Les Paul", "Roland Space Echo RE-201"}, Destination: "Vivek", Price: 3400.00}
-	orderMap["15"] = &pb.Order{Id: "15", Items: []string{"Behringer Model-D"}, Destination: "Seida Nin", Price: 330.00}
-	orderMap["16"] = &pb.Order{Id: "16", Items: []string{"Squier Jazzmaster", "Eventide Space", "EHX Big Muff"}, Destination: "Aldrun", Price: 920.00}
+	orderMap["13"] = &pb.Order{Id: "13", Items: []string{"Boss BD-2"}, Destination: "Balmora", Price: 140.00}
+	orderMap["14"] = &pb.Order{Id: "14", Items: []string{"Gibson Les Paul", "Roland Space Echo RE-201"}, Destination: "Balmora", Price: 3400.00}
+	orderMap["15"] = &pb.Order{Id: "15", Items: []string{"Behringer Model-D"}, Destination: "Seyda Neen", Price: 330.00}
+	orderMap["16"] = &pb.Order{Id: "16", Items: []string{"Squier Jazzmaster", "Eventide Space", "Boss RV-5"}, Destination: "Balmora", Price: 920.00}
 }
